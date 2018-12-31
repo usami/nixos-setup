7453bbd17372bd25d856f0ddecb72f14d228f61c
Yu Usami <yusmi05@gmail.com>
2018-12-31 22:40:17 +0900

    Add initial configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    initrd = {
+      kernelModules = [ "vfat" "nls_cp437" "nls_iso8859-1" "usbhid" ];
+      luks = {
+        cryptoModules = [ "aes" "xts" "sha512" ];
+        yubikeySupport = true;
+        devices = {
+          "crypted" = {
+            device = "/dev/nvme0n1p2";
+            preLVM = true;
+            yubikey = {
+              twoFactor = false;
+              gracePeriod = 60;
+              storage.device = "/dev/nvme0n1p1";
+            };
+          };
+        };
+      };
+    };
+  };
+
+  swapDevices = [
+    { label = "swap"; }
+  ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/root";
+      fsType = "ext4";
+      options = [ "noatime" "nodiratime" "discard" ];
+    };
+    "/boot" = {
+      device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+    };
+  };
+
+  system.stateVersion = "18.09";
+}
