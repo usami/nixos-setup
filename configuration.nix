3880a3aa633075d7568cf5665fc5997b48cf1050
Yu Usami <yu@usami.llc>
2020-02-26 15:04:09 +0900

    workaround for bootctl failure

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
   ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.canTouchEfiVariables = false;
   boot.initrd.kernelModules = [ "vfat" ];
 
   environment.systemPackages = with pkgs; [ git kbfs ];
